b52725ed8b3a4dc3bbcfb61c9052d83071a7104a
Victor Mwenda <vmwenda.vm@gmail.com>
2016-03-18 10:17:11 +0300

    GDG and WTM App
    Created multi-flavor application

---
+++ gdg/java/com/victor/apps/mmust/gdg/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.victor.apps.mmust.gdg.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.victor.apps.mmust.R;
+
+/**
+ * Created by victor on 3/18/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
